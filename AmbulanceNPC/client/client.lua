6dd3f62f492a9a7740e4390b34a9ede71830d7db
V01d <102972137+00v01d@users.noreply.github.com>
2023-09-03 22:49:25 +0200

    Add files via upload

---
+++ AmbulanceNPC/client/client.lua
@@ -0,0 +1,83 @@
+local position_x = 311.61
+local position_y = -594.13
+local position_z = 42.30
+local heading = 337.34
+
+local isAtPed = false
+
+-- Placing the ped
+CreateThread(function()
+    if not isPedLoaded then
+        RequestModel("csb_trafficwarden")
+        while not HasModelLoaded("csb_trafficwarden") do
+            Wait(10)
+        end
+    end     
+
+    local ped = CreatePed(4,"csb_trafficwarden",position_x,position_y,position_z,heading,true,false)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    FreezeEntityPosition(ped, true)
+
+    -- Set Ped health so it cant die
+    while true do
+        pedhealth = GetEntityHealth(ped)
+        SetEntityMaxHealth(ped,100000)
+
+        if pedhealth <= 99999 then 
+            SetEntityHealth(ped,100000)
+        end
+        Wait(1)
+    end
+
+
+end)
+
+
+-- Creating Info Bar
+CreateThread(function()
+    while true do
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+        local distance = Vdist(playerCoords,position_x,position_y,position_z) 
+        isAtPed = false
+        if distance < 2.0 then
+            isAtPed = true
+        end
+        Wait(1)
+    end
+
+end)
+-- Healing the player
+CreateThread(function()
+
+
+    while true do
+        local playerPed = PlayerPedId()
+        if isAtPed then
+            showInfobar('Press ~g~E~s~, to get a treatment!')
+            if IsControlJustReleased(0, 38) then
+                SetEntityHealth(playerPed,500)
+                ShowNotification('You are feeling good!')
+            end
+        end
+        Wait(1)
+    end
+end)
+
+
+
+
+
+function showInfobar(msg)
+    CurrentActionMsg  = msg
+    SetTextComponentFormat('STRING')
+    AddTextComponentString(CurrentActionMsg)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+function ShowNotification(text)
+    SetNotificationTextEntry('STRING')
+       AddTextComponentString(text)
+    DrawNotification(false, true)
+end
+   
---
+++ AmbulanceNPC/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'v01d'
+
+client_scripts {
+    '/client/client.lua'
+}
